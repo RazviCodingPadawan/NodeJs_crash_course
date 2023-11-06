690233d18a867c51c190a6c4c889f464eeb9475d
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-06 21:08:43 +0100

    middleware added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -8,6 +9,23 @@ app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// app.use((req, res, next) => {
+//   console.log("new request made:");
+//   console.log("host: ", req.hostname);
+//   console.log("path: ", req.path);
+//   console.log("method: ", req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("in the next middleware");
+//   next();
+// });
 
 app.get("/", (req, res) => {
   // res.send('<p>home page</p>');
